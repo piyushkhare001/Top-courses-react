9f156f4590e3fe8ee93b67613e335ae2584f3af0
Piyush khare <piyushkhare671@gmail.com>
2023-08-04 12:01:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React from "react";
+import Navbar from  "./components/Navbar";
+import Cards from "./components/Cards"
+import Filter from "./components/Filter"
+import { apiUrl, filterData  } from "./data";
+import { useState,useEffect } from "react";
+import Spinner from "./components/Spinner";
+import {toast} from "react-toastify";
+
+
+const App = () => { 
+  const [courses, setCourses] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [category, setCategory] = useState(filterData[0].title);
+
+  async function fetchData() {
+    setLoading(true);
+    try{
+      let response = await fetch(apiUrl);
+      let output = await response.json();
+      ///output -> 
+      setCourses(output.data);
+    }
+    catch(error) {
+        toast.error("Network me koi dikkat hai");
+    }
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchData(); 
+  }, [])
+  
+
+return (
+  <div className="min-h-screen flex flex-col bg-bgDark2">
+  <div>
+    <Navbar/>
+  </div>
+  <div className="bg-bgDark2">
+    <div>
+      <Filter 
+      filterData={filterData}
+        category={category}
+        setCategory={setCategory}
+      />
     </div>
-  );
-}
+    <div className="w-11/12 max-w-[1200px] 
+    mx-auto flex flex-wrap justify-center items-center min-h-[50vh]">
+    {
+        loading ? (<Spinner/>) : (<Cards courses={courses} category={category}/>)
+      }
+    </div>
+  </div>
 
+
+</div>
+
+
+
+)
+}
 export default App;
---
+++ src/components/Spinner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Spinner.css"
+const Spinner = () => {
+  return (
+    <div className="flex flex-col items-center space-y-2">
+      <div className='spinner'></div>
+
+    </div>
+  )
+}
+
+export default Spinner
